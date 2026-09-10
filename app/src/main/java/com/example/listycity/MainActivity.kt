3bda4675702a6cb74385e8776baefaa5e56d0afc
wesley <wesleywilliam244@gmail.com>
2026-09-09 19:27:30 -0600

    Added add city and delete city

--- app/src/main/java/com/example/listycity/MainActivity.kt
+++ app/src/main/java/com/example/listycity/MainActivity.kt
@@ -4,30 +4,127 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.listycity.ui.theme.ListyCityTheme
 
+var selectedDeletedCity = ""
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val cityRepository = CityRepository()
         setContent {
             ListyCityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    CityListScreen(
+                        cities = cityRepository.cities,
+                        onAddCity = {cityRepository.addCity(it)},
+                        onDeleteCity = {cityRepository.deleteCity(it)},
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+}
+
+@Composable
+fun CityListScreen(
+    cities: List<String>,
+    onAddCity: (String) -> Unit,
+    onDeleteCity: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var newCityName by remember { mutableStateOf(value = "") }
+    Column(modifier = Modifier.fillMaxSize().padding(top = 40.dp)) {
+        Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+            OutlinedTextField(
+                value = newCityName,
+                onValueChange = { newCityName = it },
+                label = { Text("CityName") },
+                modifier = Modifier.weight(1f)
+                    .height(100.dp)
+            )
+
+            Spacer(modifier = Modifier.width((8.dp)))
+
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {
+                        if (newCityName.isNotBlank()) {
+                            onAddCity(newCityName)
+                            newCityName = ""
+                        }
+                    }
+                ) { Text("Add City") }
+
+                Button(
+                    onClick = {
+                        if (selectedDeletedCity.isNotBlank()) {
+                            onDeleteCity(selectedDeletedCity)
+                            selectedDeletedCity = ""
+                        }
+                    }
+                ) { Text("Delete Last Clicked City") }
+            }
+
+        }
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(cities) { city ->
+                CityRow(city = city)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun CityRow(city: String) {
+    Row(
+        modifier = Modifier.fillMaxSize()
+            .clickable {
+                selectedDeletedCity = city
+            }
+            .padding(horizontal = 18.dp, vertical = 14.dp)
+    ) {
+        Text(
+        text = city,
+        fontSize = 28.sp,
+//        modifier = Modifier.fillMaxWidth()
+//            .padding(horizontal = 18.dp, vertical = 14.dp)
+        )
+    }
+
 }
 
 @Composable
@@ -43,5 +140,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ListyCityTheme {
         Greeting("Android")
+    }
+}
+
+class CityRepository {
+    private val _cities = mutableStateListOf(
+        "Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna",
+        "Tokyo", "Beijing", "Osaka", "New Delhi"
+    )
+
+    val cities: List<String>
+        get() = _cities
+
+    fun addCity(city: String) {
+        _cities.add(city)
+    }
+
+    fun deleteCity(city: String) {
+        _cities.remove(city)
     }
 }
